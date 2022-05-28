0f9fe93c23e9bb1ade866f2fe52c228bd815dcb5
Kelly Park <sehyun.kelly.park@gmail.com>
2022-05-27 17:27:31 -0700

    timestamp updated

--- content/templatetags/content_tags.py
+++ content/templatetags/content_tags.py
@@ -6,8 +6,34 @@ register = template.Library()
 
 @register.filter
 @stringfilter
-def upto(value, delimiter=None):
-    return value.split(delimiter)[0]
+def upto(value):
+    result = '';
 
+    if 'year' in value:
+        result = value.split('year')[0]
+        if '1' in result and len(result) == 2:
+            return result + 'YEAR'
+        else:
+            return result + 'YEARS'
+
+    if 'day' in value:
+        result = value.split('day')[0]
+        if '1' in result and len(result) == 2:
+            return result + 'DAY'
+        else:
+            return result + 'DAYS'
+
+    if 'hour' not in value and 'minute' in value:
+        result = value.split('minute')[0]
+        if '1' in result and len(result) == 2:
+            return result + 'MINUTE'
+        else:
+            return result + 'MINUTES'
+
+    result = value.split('hour')[0]
+    if '1' in result and len(result) == 2:
+        return result + 'HOUR'
+    else:
+        return result + 'HOURS'
 
 upto.is_safe = True
--- content/views.py
+++ content/views.py
@@ -11,7 +11,7 @@ from .models import Feed
 # Create your views here.
 class Main(APIView):
     def get(self, request):
-        feed_list = Feed.objects.all().order_by('-id') #select * from content_feed;
+        feed_list = Feed.objects.all().order_by('-created') #select * from content_feed;
 
         return render(request, "Instagram/main.html", context=dict(feeds=feed_list))
 
